4b580fd51bfc28c237ff523a8cb2cf8f2ef463ef
Toufiq Hasan  Kiron <toufiqhasankiron0@gmail.com>
2022-10-07 10:47:48 +0600

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png' className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link underline hover:no-underline hover:text-white"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link underline hover:no-underline hover:text-white"
+          href="https://github.com/THK-S-SOFTWARE"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow me on GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
